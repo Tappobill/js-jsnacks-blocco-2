94061d2f4db9337add7f8406bed57455129997ba
Tappobill <christian.salvati@hotmail.it>
2022-10-25 15:48:03 +0200

    Snack1

---
+++ Snack1/jscript/main.js
@@ -0,0 +1,12 @@
+// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo ( del numero inserito dall'utente: esempio se il numero dell'utente è 2, il numero successivo è 3  ).
+
+let numero =parseInt(prompt("Scrivi un numero"))
+if (numero % 2 == 0) {
+    console.log(numero)
+}else {
+    console.log(numero + 1)
+}
+
+
+// Generatore di “nomi cognomi” casuali del grande gatsby:
+// Abbiamo una lista di nomi e una lista di cognomi, e da queste vogliamo generare una "falsa" lista di invitati, dove ogni invitato nella lista, è l'unione di un "nome" e un "cognome" delle liste precedentemente dette, scelti in modo casuale.
