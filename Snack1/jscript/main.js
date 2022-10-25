2f33b8d75238082b4dfd312986c14f47550b77c1
Tappobill <christian.salvati@hotmail.it>
2022-10-25 16:16:42 +0200

    Virgolette

--- Snack1/jscript/main.js
+++ Snack1/jscript/main.js
@@ -1,9 +1,9 @@
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo ( del numero inserito dall'utente: esempio se il numero dell'utente è 2, il numero successivo è 3  ).
 
-let numero =parseInt(prompt("Scrivi un numero"))
+let numero =parseInt(prompt("Scrivi un numero"));
 if (numero % 2 == 0) {
-    console.log(numero)
+    console.log(numero);
 }else {
-    console.log(numero + 1)
+    console.log(numero + 1);
 }
 
